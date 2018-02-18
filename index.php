dddd963cee7f7bfc8ca6a21fe535a0ca7aeb67b4
Steffen J Gundersen <steffenjg94@gmail.com>
2018-02-18 18:47:09 +0100

    Fikset bakgrunn, kommentarer i index.php og sass struktur

--- index.php
+++ index.php
@@ -1,9 +1,8 @@
 <?php
-include('php/login.php'); // Includes Login Script
+include('php/login.php'); // Inkluderer Login Script
 
 if(isset($_SESSION['login_user'])){
-header("location: profile.php");
-}
+header("location: profile.php");}
 ?>
 <!doctype html>
 <html lang="en">
@@ -12,45 +11,46 @@ header("location: profile.php");
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <meta name="description" content="">
     <meta name="author" content="">
-
     <title>Tronrud</title>
-
-    <!-- Custom styles for this template -->
+    <!-- Custom CSS -->
     <link href="Sass/main.css" rel="stylesheet">
-
+    <!-- Bootstrap 4 -->
     <link href="node_modules/bootstrap/dist/css/bootstrap.css" rel="stylesheet">
-
+    <!-- Fontawesome 5 -->
     <script defer src="static/fontawesome/fontawesome-all.js"></script>
   </head>
 
   <body class="text-center">
+  <div id="bg">
+    <img src="img/TronrudBackground.jpg">
+  </div>
+    <!-- Login form -->
     <form class="form-signin" method="post" action="">
       <img class="mb-4" src="img/tronrud-engineering-logo-svart.svg" alt="" width="320" height="160">
-      <h1 class="h3 mb-3"></h1>
-
+      <!-- Username input -->
       <div class="input-group mb-3">
         <div class="input-group-prepend">
           <span class="input-group-text"><i class="fas fa-user"></i></span>
         </div>
           <input type="text" name="username" id="inputEmail" class="form-control" placeholder="Username" required autofocus>
       </div>
-      
+      <!-- Password input -->
       <div class="input-group mb-3">
           <div class="input-group-prepend">
             <span class="input-group-text"><i class="fas fa-lock"></i></span>
           </div>
           <input type="password" name="password" id="inputPassword" class="form-control" placeholder="Password" required>
         </div>
-      
+      <!-- "Remember me" -->
       <div class="custom-control custom-checkbox mb-3 text-left">
         <input type="checkbox" class="custom-control-input" id="customCheck1">
         <label class="custom-control-label" for="customCheck1"></label>Remember me</label>
       </div>
-
+      <!-- Innloggingsknappen -->
       <button class="btn btn-lg btn-primary btn-block" type="submit" name="submit">Sign in</button>
-      
+      <!-- Feilmelding -->
       <span><?php echo $error; ?></span>
-      
+      <!-- Copyright -->
       <p class="mt-5 mb-3">&copy; 2018 Tronrud Engineering</p>
     </form>
   </body>
