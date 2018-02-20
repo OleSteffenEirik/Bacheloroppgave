36f2d562322a783c7ec36662f48d48158bf0a47a
Ole Martin Helgesen <ohelgesen39@gmail.com>
2018-02-20 19:38:19 +0100

    gjorde function() om til =>-funksjon isteden, og endret variabel-definisjon

--- js/functions.js
+++ js/functions.js
@@ -1,4 +1,5 @@
 /* Script for count ned/ teller ned tiden for time-out */
+/*
 function startTimer(duration, display) {
     var timer = duration, minutes, seconds;
     var end =setInterval(function () {
@@ -23,3 +24,32 @@ window.onload = function () {
         display = document.querySelector('#counter');
     startTimer(fiveMinutes, display);
 };
+*/
+
+
+/* Script for count ned/ teller ned tiden for time-out */
+const startTimer =(duration, display) => {
+    let timer = duration, minutes, seconds;
+    let end =setInterval(()=> {
+        minutes = parseInt(timer / 60, 10)
+        seconds = parseInt(timer % 60, 10);
+
+        minutes = minutes < 10 ? "0" + minutes : minutes;
+        seconds = seconds < 10 ? "0" + seconds : seconds;
+
+        display.textContent = minutes + ":" + seconds;
+        //display.textContent = seconds;
+
+        if (--timer < 0) {
+            window.location = "../index.php";
+            clearInterval(end);
+        }
+    }, 1000);
+}
+
+
+// ES6 sin Arrow-funksjon.
+ window.onload = () => {
+    let fiveMinutes = 120, display = document.querySelector('#counter'); startTimer(fiveMinutes, display)
+};
+
