dd7438812e48f182465a413d59313bccf4feb430
EirikAanstad <Eaanstad@csumb.edu>
2018-02-23 10:52:14 +0100

    fix

--- php/search.php
+++ php/search.php
@@ -12,4 +12,18 @@
     $records = $stmt->fetchAll(PDO::FETCH_ASSOC);
     
     echo json_encode($records);
+
+    // require_once "connect.php";
+    // $con = new tronrudDB();
+
+    // $name = $con->real_escape_string("%".$_GET['name']."%");
+
+    // $sql = "SELECT name, Item_Id
+    //                     FROM products
+    //                     WHERE name LIKE '$name'";
+
+    // $stmt = $con->query($sql);
+    // $records = MYSQLI_FETCH_ALL($stmt, MYSQLI_ASSOC);
+    
+    // echo json_encode($records)
 ?>
