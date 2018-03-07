3feb6cc1dc2ab0f0034f94b770f7fc05744057a5
Steffen J Gundersen <steffenjg94@gmail.com>
2018-03-07 13:14:20 +0100

    Redesignet navbar

--- php/navbar.php
+++ php/navbar.php
@@ -1,9 +1,9 @@
-<nav class="navbar">
-<img class="" src="../img/tronrud-engineering-logo-svart.svg" alt="logo" width="200px">
-
-
-    <!--Logout--> 
-    <a class="button" href="../php/includes/logout.php"><i class="fas fa-sign-out-alt"></i>Logout</a>
-    
-
+<nav class="navbar trondrud-primary">
+    <div class="container d-flex justify-content-between">
+        <img class="img-responsive" src="../img/tronrud-engineering-logo.svg" alt="logo" height="40px" width="200px">
+        <!--Logout--> 
+        <a class="btn btn-md btn-tronrud-primary" href="../php/includes/logout.php">
+            <i class="fas fa-sign-out-alt fa-lg align-middle mr-1"></i><b>Sign out</b>
+        </a>
+    </div>
 </nav>
